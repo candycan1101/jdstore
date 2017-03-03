4f769df67528b7dacf0925b1819a3dd797b20505
candy <candycan@163.com>
2017-03-03 21:28:40 +0800

    revise seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,22 +13,22 @@ u.password_confirmation = "123456"
 u.is_admin = true
 u.save
 
-Product.create!(title:"多功能椅"，
-                description:"瞬间进入心流状态的神奇座椅"，
+Product.create!(title: "多功能椅",
+                description: "瞬间进入心流状态的神奇座椅",
                 price: 12000,
                 quantity: 20,
                 image: open("https://ww2.sinaimg.cn/large/006tNc79gy1fd9y9t6h4fj30m80m8gmm.jpg")
                 )
 
-Product.create!(title:"简洁型书桌"，
-                description:"带侧柜简洁型书桌"，
+Product.create!(title: "简洁型书桌",
+                description: "带侧柜简洁型书桌",
                 price: 560,
                 quantity: 20,
                 image: open("https://ww3.sinaimg.cn/large/006tNc79gy1fd9yccmf5rj30ac0ac746.jpg")
                 )
 
-Product.create!(title:"小圆桌"，
-                description:"直径80cm的小圆桌"，
+Product.create!(title: "小圆桌",
+                description: "直径80cm的小圆桌",
                 price: 400,
                 quantity: 20,
                 image: open("https://ww4.sinaimg.cn/large/006tNc79gy1fd9yjzzdroj30ci0ciwec.jpg")
